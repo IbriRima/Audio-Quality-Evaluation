438e694c04f3eb565dcc798b2d2fc3d35ea391d9
Rima Ibri <rima.ibri@esprit.tn>
2023-12-02 11:11:51 +0100

    Set up action

--- main.py
+++ main.py
@@ -12,6 +12,30 @@ import glob
 import argparse
 from pymongo import MongoClient
 from bson.binary import Binary
+import logging
+import logging.handlers
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
+try:
+    SOME_SECRET = os.environ["SOME_SECRET"]
+except KeyError:
+    SOME_SECRET = "Token not available!"
+
+
+
+
 
 def json_data(json_file_path):
     if not os.path.exists(json_file_path):
