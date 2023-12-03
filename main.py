04d9eee1ed8a8966058790d0dd7506bdb4bcd78f
Rima Ibri <rima.ibri@esprit.tn>
2023-12-03 17:01:00 +0100

    Update main script

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-import librosa
-import numpy as np
-import pandas as pd
+import os
 import json
 import requests
-import os
+import logging
+import logging.handlers
+import argparse
+from bson.binary import Binary
+
+import numpy as np
+import pandas as pd
+import plotly.express as px
 import matplotlib.pyplot as plt
 import IPython.display as ipd
 from sklearn.cluster import KMeans
-import argparse
-import glob
-import argparse
+import librosa
+
 from pymongo import MongoClient
-from bson.binary import Binary
-import logging
-import logging.handlers
+
 
 
 logger = logging.getLogger(__name__)
@@ -127,6 +129,15 @@ def display_audio (folder_path,save_folder='./plots'):
 def display_audio(path):
     ipd.Audio(path)
 
+def get_all_records(directory_path):
+    files_and_directories = os.listdir(directory_path)
+    records_list=[]
+    for item in files_and_directories:
+        full_path = os.path.join(directory_path, item)
+        if os.path.isfile(full_path):
+            records_list.append(item)
+    return records_list
+
 def estimate_noise(signal, speech_estimate):
     # Estimate noise by subtracting the estimated speech from the original signal
     noise_estimate = signal - speech_estimate
@@ -168,7 +179,17 @@ def calculate_thd(audio_signal, fundamental_frequency):
     
     return thd
 
-def estimate_audio_quality(file_path):
+def calculate_dynamic_range_db( audio,rms_audio):
+    peak_amplitude = np.max(np.abs(audio))
+    dynamic_range_db = 20 * np.log10(peak_amplitude / rms_audio)
+    return dynamic_range_db
+
+def calculate_speech_level(audio,sample_rate):
+    spectral_centroid = librosa.feature.spectral_centroid(y=audio, sr=sample_rate)[0]
+    speech_level = np.mean(spectral_centroid)
+    return speech_level
+
+def audio_features(file_path):
     try:
         # Load audio file
         audio, sample_rate = librosa.load(file_path, sr=None)
@@ -176,14 +197,13 @@ def estimate_audio_quality(file_path):
         # Calculate audio RMS
         rms_audio = calculate_rms(audio)
 
-        peak_amplitude = np.max(np.abs(audio))
-        dynamic_range_db = 20 * np.log10(peak_amplitude / rms_audio)
+        #Calculate dynamic range of the audio signal in decibels
+        dynamic_range_db=calculate_dynamic_range_db( audio,rms_audio)
 
         # Calculate Speech Level
-        spectral_centroid = librosa.feature.spectral_centroid(y=audio, sr=sample_rate)[0]
-        speech_level = np.mean(spectral_centroid)
+        speech_level = calculate_speech_level(audio,sample_rate)
 
-        #speech estimate
+        # Pre-emphasis to the audio signal using a pre-emphasis filter
         speech_estimate = librosa.effects.preemphasis(audio)
 
         # Estimate noise
@@ -194,17 +214,30 @@ def estimate_audio_quality(file_path):
 
         # Calculate SNR ratio
         snr=calculate_snr(rms_audio,rms_noise)
-        return speech_level, snr,rms_noise,sample_rate,dynamic_range_db
+
+        return speech_level, snr,rms_noise,sample_rate,dynamic_range_db,rms_audio
     
     except Exception as e:
         print(f"Error loading audio file: {e}")
         return None
 
-
+def create_dataframe(csv_file,mp3_directory='./mp3_records/'):
+    data_list=[]
+    records_list=get_all_records(mp3_directory)
+    for record in records_list:
+        info_quality = audio_features('./mp3_records/' + record)
+        if info_quality is not None:
+            data_list.append({'record': record, 'speech_level': info_quality[0], 'snr': info_quality[1],\
+                            'rms_noise':info_quality[2],'sample_rate':info_quality[3],\
+                            'dynamic_range_db':info_quality[4],'rms_audio':info_quality[5]})
+    data=pd.DataFrame(data_list) 
+    os.makedirs('./data', exist_ok=True)
+    data.to_csv('./data/'+csv_file,index=False)
+    return data
 
 def data_storage(data):
-    # Connect to MongoDB
 
+    # Connect to MongoDB
     db = client[mongo_db]
     metadata_collection = db['audios']
 
@@ -230,28 +263,127 @@ def data_storage(data):
         # Insert the document into the MongoDB collection
         metadata_collection.insert_one(record_document)
 
+# To be sure that it will be relevant to have 2 classes for our dataset we will be using the elbow method
+def elbow_method(data_clustering,max_iter,n_init):
+    num_clusters_range = range(1, 11)  # Try clusters from 1 to 10
+    sse = [] 
+    for num_clusters in num_clusters_range:
+        kmeans = KMeans(n_clusters=num_clusters, random_state=42,n_init=n_init,max_iter=max_iter)
+        kmeans.fit(data_clustering)
+        sse.append(kmeans.inertia_)
+
+    plt.plot(num_clusters_range, sse, marker='o')
+    plt.title('Elbow Plot')
+    plt.xlabel('Number of Clusters (K)')
+    plt.ylabel('Sum of Squared Distances')
+
+    # Display the plot
+    plt.show()
+
+    # Save the plot
+    plt.savefig('./plots/elbow_plot.png')
+
+
+def clustering(data,max_iter,n_init,n_clusters=2):
+    kmeans = KMeans(n_clusters=n_clusters, random_state=42,n_init=n_init,max_iter=max_iter)
+    kmeans.fit(data)
+    cluster_centers = kmeans.cluster_centers_
+    labels = kmeans.labels_
+    data['label']=labels
+    plt.scatter(data_cluster.iloc[:, 0], data_cluster.iloc[:, 1], c=labels, cmap='viridis', edgecolors='k', alpha=0.7)
+    plt.scatter(cluster_centers[:, 0], cluster_centers[:, 1], c='red', marker='X', s=20, label='Cluster Centers')
+    plt.title('K-Means Clustering')
+    plt.xlabel('rms_noise')
+    plt.ylabel('speech_level')
+    plt.legend()
+
+    # Save the plot
+    plt.savefig("./plots/kmeans_clustering_plot.png", dpi=300, bbox_inches='tight')
+
+    # Display the plot
+    plt.show()
+
+    return data
+    
+
+
+def labels_pie_plot(data_cluster):
+    # Count the occurrences of each unique value in the 'label' column
+    label_counts = data_cluster['label'].value_counts()
+
+    # Create a Pie chart using Plotly
+    fig = px.pie(
+        label_counts,
+        values=label_counts,
+        names=label_counts.index,
+        title='Pie Plot for Cluster Labels',
+        labels={'label': 'Cluster Labels'},
+        template='plotly',
+    )
+
+    # Save the plot as an a png image
+    fig.write_image("./plots/Labels_pie_plot.png", format='png', scale=4)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("action", choices=["mp3", "audio_viz", "save_data", "elbow_method", "audios_classification"],
+                        help="Choose action: mp3, audio_viz, save_data, elbow_method, or audios_classification")
+
+    # Add optional parameters for audios_classification
+    parser.add_argument("--features", nargs="+", help="Provide the list of features for clustering")
+
+    args = parser.parse_args()
+
+    return args
+
+
 if __name__ == "__main__":
+
     # paths
     json_file_path = "./speechcrawling/records.json"
     mp3_folder = './mp3_records'
     viz_folder='./plots'
-
-    # args
-    parser = argparse.ArgumentParser()
-    parser.add_argument("action", choices=["mp3", "audio_viz","save_data"], help="Choose action: mp3 or audio_viz")
-    
-    args = parser.parse_args()
+ 
+    args =parse_args()
 
     if args.action == "mp3":
-        print(f"Downloading mp3 files from {json_file_path}")
+        logger.info(f"Downloading mp3 files from {json_file_path}")
+
     elif args.action == "audio_visualization":
-        print("Performing audio visualizations")
+        logger.info("Performing audio visualizations")
         display_audio (mp3_folder,save_folder='./plots')
+
+    elif args.action=="create_dataframe":
+        data=create_dataframe(mp3_directory='./mp3_records/')
     elif args.action== "save_data":
-        print("Inserting records into  MongoDB database")
+        logger.info("Inserting records into MongoDB database")
         data=pd.read_csv('./records.csv')
         data_storage(data)
+
+    elif args.action== "elbow_method":
+        logger.info("Inserting records into MongoDB database")
+        data=pd.read_csv('./records.csv')
+        data_clustering=data[['rms_noise','speech_level','dynamic_range_db']]
+        elbow_method(data_clustering)
+
+    elif args.action == "audios_classification" and args.features:
+        logger.info("Classifying audios based on their quality")
+        features_to_use = args.features
+        data = pd.read_csv('./records.csv')
+
+        # Check if specified features exist in the dataset
+        invalid_features = [feature for feature in features_to_use if feature not in data.columns]
+        if invalid_features:
+            logger.warning(f"Invalid feature(s) specified: {', '.join(invalid_features)}")
+        else:
+            data_cluster = data[features_to_use]
+            data_labeled = clustering(data_cluster, max_iter=15, n_init='auto', n_clusters=2)
+            data_labeled.to_csv('Classified_audio.csv', index=False)
+
+
+
     else:
-        print("Invalid action provided.")
+        logger.warning("Invalid action provided.")
 
         
